d59c63bc310dbdce970cba0c5c9b6549a8ef69b4
Steven <scroft001@gmail.com>
2023-03-08 17:38:36 -0800

    Started summary section

--- results-summary/src/Result.jsx
+++ results-summary/src/Result.jsx
@@ -5,8 +5,11 @@ const Result = () => {
     <section className="result">
         <h1>Result</h1>
         <div className='average'>
-
+            <h2>72</h2>
+            <p>of 100</p>
         </div>
+        <h2>Great</h2>
+        <p>You scored higher than 65% of the people who have taken these tests.</p>
     </section>
   )
 }
--- results-summary/src/Summary.jsx
+++ results-summary/src/Summary.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import {personalData} from "./data"
 
 const Summary = () => {
   return (
     <section className="summary">
-        <p>Summary</p>
+        <h2>Summary</h2>
+            {personalData.map((data, index) => {
+                return (
+                    <div className='summaryItem' id={data.category} key={index}>
+                        <img src={data.icon} alt={data.category} />
+                        <h3>{data.category}</h3>
+                        <p>{data.score}<span>/100</span></p>
+                    </div>
+                )
+            })}
     </section>
   )
 }
